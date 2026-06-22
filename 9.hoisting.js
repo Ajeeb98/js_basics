81e7aeacc63ec4b9f8c513ee248dacb89a04690a
Ajeeb Maharjan <ajeeb0987@gmail.com>
2026-06-22 14:25:16 +0545

    hoisting

---
+++ 9.hoisting.js
@@ -0,0 +1,24 @@
+//HOISTING
+
+//var x
+
+// console.log(x);
+// var x = 10;
+// //x= 10
+// console.log(x);
+
+// function declaration
+// hoist();
+// function hoist() {
+//     console.log("Hoisting");
+// }
+
+// function expression
+//a(); //! TypeError: a is not a function
+// var a = function() {
+//     console.log("a");
+
+// }
+
+console.log(b); //! undefined
+const b = 20;
